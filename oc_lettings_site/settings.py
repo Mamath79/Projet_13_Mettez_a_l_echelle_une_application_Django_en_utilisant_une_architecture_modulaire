f9b0f52eb4fb6993fac192744e3f144d35ea3934
Mathieu Vieillefont <57585607+Mamath79@users.noreply.github.com>
2025-03-26 12:34:28 +0100

    Complet V3

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware", 
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,8 +129,6 @@ if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-
-
 
 sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),  # Stocké dans les variables d'environnement
